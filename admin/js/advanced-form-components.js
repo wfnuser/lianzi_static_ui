088569c5811c1790d8d0c364febf04dfa2a7093c
Scott Li <scottliyq@gmail.com>
2016-03-12 15:05:01 +0800

    add cdo setup

--- admin/js/advanced-form-components.js
+++ admin/js/advanced-form-components.js
@@ -182,7 +182,7 @@ $('.colorpicker-rgba').colorpicker();
 //spinner start
 $('#spinner1').spinner();
 $('#spinner2').spinner({disabled: true});
-$('#spinner3').spinner({value:0, min: 0, max: 10});
+$('#spinner3').spinner({value:0, min: 0, max: 2});
 $('#spinner4').spinner({value:0, step: 5, min: 0, max: 200});
 //spinner end
 
